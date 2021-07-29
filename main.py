4bf6ee84b676ca69def058b68e7799bc68d9e496
KPZNET <kpznet@outlook.com>
2021-07-29 10:36:54 -0700

    mac dev

--- main.py
+++ main.py
@@ -33,34 +33,22 @@ def EditDistanceDynamic(str1, str2, m, n):
 
 def EditDistanceRecursive(str1, str2, m, n):
 
-    # If first string is empty, the only option is to
-    # insert all characters of second string into first
     if m == 0:
         return n
 
-    # If second string is empty, the only option is to
-    # remove all characters of first string
     if n == 0:
         return m
 
-    # If last characters of two strings are same, nothing
-    # much to do. Ignore last characters and get count for
-    # remaining strings.
     if str1[m-1] == str2[n-1]:
         return EditDistanceRecursive(str1, str2, m-1, n-1)
 
-    # If last characters are not same, consider all three
-    # operations on last character of first string, recursively
-    # compute minimum cost for all three operations and take
-    # minimum of three values.
-    return 1 + min(EditDistanceRecursive(str1, str2, m, n-1), # Insert
-                EditDistanceRecursive(str1, str2, m-1, n), # Remove
-                EditDistanceRecursive(str1, str2, m-1, n-1) # Replace
+    return   min(20 + EditDistanceRecursive(str1, str2, m, n-1), #Insert a character
+                 20 + EditDistanceRecursive(str1, str2, m-1, n), # Remove a character
+                 5 + EditDistanceRecursive(str1, str2, m-1, n-1) # Replace a character
                 )
 
-
-# Driver code
-str1 = "horizon"
-str2 = "hxrizontal"
+#Main Runline
+str1 = "algorithm"
+str2 = "alligator"
 print (EditDistanceRecursive(str1, str2, len(str1), len(str2)))
 print(EditDistanceDynamic(str1, str2, len(str1), len(str2)))
