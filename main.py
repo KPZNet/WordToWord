c8222a2906e6fdc643a296a9a8939efa1396af29
Kenneth Ceglia <KPZNet@outlook.com>
2021-07-31 13:21:00 -0700

    alien devs

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+from typing import List
 
 import numpy as np
 from pkprint import ndtotext
@@ -6,58 +7,60 @@ INSERT_COST = 2
 DELETE_COST = 2
 REPLACE_COST = 1
 
-def WordTransformCost_DP(wordFrom, wordTo, m, n) ->int:
 
-    wordTransferScoreMatrix = InitializeScoreMatrix(m, n)
+def transform_word_dynamic_programming(wordFrom, wordTo, m, n) -> int:
+    word_transfer_score_matrix = initialize_score_matrix(m, n)
 
     for i in range(m + 1):
         for j in range(n + 1):
 
             if i == 0:
-                wordTransferScoreMatrix[i][j] = j * INSERT_COST
+                word_transfer_score_matrix[i][j] = j * INSERT_COST
 
             elif j == 0:
-                wordTransferScoreMatrix[i][j] = i * INSERT_COST
+                word_transfer_score_matrix[i][j] = i * INSERT_COST
 
             elif wordFrom[i - 1] == wordTo[j - 1]:
-                wordTransferScoreMatrix[i][j] = wordTransferScoreMatrix[i-1][j-1]
+                word_transfer_score_matrix[i][j] = word_transfer_score_matrix[i - 1][j - 1]
 
             else:
-                UpdateScoreMatrix(i, j, wordTransferScoreMatrix)
+                update_score_matrix(i, j, word_transfer_score_matrix)
 
-    print(ndtotext(np.array ( wordTransferScoreMatrix )))
-    return wordTransferScoreMatrix[m][n]
+    return word_transfer_score_matrix[m][n], word_transfer_score_matrix
 
-def InitializeScoreMatrix(m, n):
-    wordTransferScoreMatrix = [[0 for x in range(n + 1)] for x in range(m + 1)]
-    return wordTransferScoreMatrix
 
-def UpdateScoreMatrix(i, j, wordTransferScoreMatrix):
-    insert_cost = INSERT_COST + wordTransferScoreMatrix[i][j - 1]
-    remove_cost = DELETE_COST + wordTransferScoreMatrix[i - 1][j]
-    replacement_cost = REPLACE_COST + wordTransferScoreMatrix[i - 1][j - 1]
+def initialize_score_matrix(m: int, n: int) -> List[List[int]]:
+    word_transfer_score_matrix: List[List[int]] = [[0 for x in range(n + 1)] for x in range(m + 1)]
+    return word_transfer_score_matrix
+
+
+def update_score_matrix(i, j, word_transform_score_matrix):
+    insert_cost = INSERT_COST + word_transform_score_matrix[i][j - 1]
+    remove_cost = DELETE_COST + word_transform_score_matrix[i - 1][j]
+    replacement_cost = REPLACE_COST + word_transform_score_matrix[i - 1][j - 1]
     min_cost = min(insert_cost, remove_cost, replacement_cost)
-    wordTransferScoreMatrix[i][j] = min_cost
+    word_transform_score_matrix[i][j] = min_cost
 
 
-def WordTransformCost_Recursive(str1, str2, m, n) -> int:
-
+def transform_word_recursive(str1, str2, m, n) -> int:
     if m == 0:
-        return n* INSERT_COST
+        return n * INSERT_COST
 
     if n == 0:
-        return m* INSERT_COST
+        return m * INSERT_COST
 
-    if str1[m-1] == str2[n-1]:
-        return WordTransformCost_Recursive( str1, str2, m - 1, n - 1 )
+    if str1[m - 1] == str2[n - 1]:
+        return transform_word_recursive(str1, str2, m - 1, n - 1)
 
-    return   min( 1 + WordTransformCost_Recursive( str1, str2, m, n - 1 ),  #INSERT
-                  1 + WordTransformCost_Recursive( str1, str2, m - 1, n ),  #REMOVE
-                  2 + WordTransformCost_Recursive( str1, str2, m - 1, n - 1 )  #REPLACE
-                  )
+    return min(1 + transform_word_recursive(str1, str2, m, n - 1),  # INSERT
+               1 + transform_word_recursive(str1, str2, m - 1, n),  # REMOVE
+               2 + transform_word_recursive(str1, str2, m - 1, n - 1)  # REPLACE
+               )
 
-#Main Runline
+
+# Main Runline
 wordOne = "AB"
 wordTwo = "CDF"
-print ( WordTransformCost_Recursive( wordOne, wordTwo, len( wordOne ), len( wordTwo ) ) )
-print( WordTransformCost_DP( wordOne, wordTwo, len( wordOne ), len( wordTwo ) ) )
+# print (transformword_recursive(wordOne, wordTwo, len(wordOne), len(wordTwo)))
+total_score, score_matrix = transform_word_dynamic_programming(wordOne, wordTwo, len(wordOne), len(wordTwo))
+print(ndtotext(np.array(score_matrix)))
