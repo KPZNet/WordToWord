74ff75db9a19f36fe04edc2fcbd046e7b637d10e
KPZNET <kpznet@outlook.com>
2021-08-02 08:46:57 -0700

    mac dev

---
+++ test_main.py
@@ -0,0 +1,11 @@
+from unittest import TestCase
+
+
+class Test ( TestCase ) :
+    def test_initialize_trace_matrix(self) :
+        self.fail ()
+
+
+class Test ( TestCase ) :
+    def test_initialize_score_matrix(self) :
+        self.fail ()
